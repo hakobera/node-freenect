1fa797ec03df01b2c2ceda05a8c2050e8e1278a9
Kazuyuki Honda <hakobera@gmail.com>
2011-04-23 21:57:00 +0900

    Fix example's require path for freenect_bindigs

--- example/hellokinect/hellokinect.js
+++ example/hellokinect/hellokinect.js
@@ -1,5 +1,5 @@
-var Freenect = require('./build/default/nodefreenect').Freenect;
-var f = new Freenect();
-f.init();
-f.start();
-f.stop();
+freenect = require('../../build/default/freenect_bindings');
+var kinect = new freenect.Freenect();
+kinect.init();
+kinect.start();
+kinect.stop();
